68624d4ed1c7783f42a08fe99ba99ee6280a79b1
sevdas <sshukrieva@gmail.com>
2021-07-23 00:01:53 +0100

    get api data

--- src/Context.js
+++ src/Context.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const MyContext = React.createContext();
 
 function ContextProvider({ children }) {
-  return <MyContext.Provider value={""}>{children}</MyContext.Provider>;
+  const [allPhotos, setAllPhotos] = useState([]);
+
+  const ACCESS_KEY = process.env.REACT_APP_ACCESS_KEY;
+  const url = `https://api.unsplash.com//search/photos?query=arts-culture&client_id=${ACCESS_KEY}`;
+
+  // As soon as the ContextProvider component renders, get the JSON data from the api.
+  //Save data to state
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch(url);
+      const data = await response.json();
+      setAllPhotos(data);
+    };
+    getData();
+  }, []);
+
+  console.log(allPhotos);
+
+  return (
+    <MyContext.Provider value={{ allPhotos }}>{children}</MyContext.Provider>
+  );
 }
 
 export { ContextProvider, MyContext };
